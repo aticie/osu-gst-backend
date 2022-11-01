9029165bfad1a3c035815e6e92fb08f36f7bd00f
aticie <efehanatici@gmail.com>
2022-11-01 23:17:06 +0100

    Remove multiple workers

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
-    uvicorn.run("app:app", host='0.0.0.0', port=port, workers=2)
+    uvicorn.run("app:app", host='0.0.0.0', port=port)
