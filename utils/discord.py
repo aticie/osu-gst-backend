e1bf81e363786e3cb0496cfbcaa61e78a16e86f5
aticie <efehanatici@gmail.com>
2022-11-13 03:38:04 +0100

    Add discord api link to user_join_guild

--- utils/discord.py
+++ utils/discord.py
@@ -7,7 +7,7 @@ async def user_join_guild(user_id: str, access_token: str):
     headers = {"Authorization": f"Bearer {access_token}"}
     guild_id = os.getenv("DISCORD_GUILD_ID")
     async with aiohttp.ClientSession(headers=headers) as sess:
-        async with sess.put(f"/guilds/{guild_id}/members/{user_id}") as resp:
+        async with sess.put(f"https://discord.com/api/10/guilds/{guild_id}/members/{user_id}") as resp:
             if resp.status != 200:
                 print(f"Discord returned non-200: {resp.status}")
                 resp_text = await resp.text()
